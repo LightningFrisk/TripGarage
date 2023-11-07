335c62351cd4e2167db6f8b0ad0ab5143f515340
LightningFrisk <bmxcaleb@gmail.com>
2023-11-07 12:12:03 -0600

    Setting up Spring
    Setting up Spring

---
+++ src/main/java/com/lightningfrisk/tripgarage/TripgarageApplication.java
@@ -0,0 +1,13 @@
+package com.lightningfrisk.tripgarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripgarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripgarageApplication.class, args);
+	}
+
+}
